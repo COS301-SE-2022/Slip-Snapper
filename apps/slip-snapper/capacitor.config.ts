40c32cd880a3a1fdd71937a3d5608abf74b86a1d
Regan <u20556455@tuks.co.za>
2022-05-01 18:30:52 +0200

    :sparkles: (repo): Created workspace

---
+++ apps/slip-snapper/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'slip-snapper',
+  webDir: '../../dist/apps/slip-snapper',
+  bundledWebRuntime: false,
+};
+
+export default config;
