add96667e030f74142bc060dc5f81360f4a7629b
Regan <u20556455@tuks.co.za>
2022-05-28 16:42:29 +0200

    :sparkles: (ocr): Used Tesseract Text

--- frontend/apps/slip-snapper/src/app/components/TakePictureButton.tsx
+++ frontend/apps/slip-snapper/src/app/components/TakePictureButton.tsx
@@ -7,16 +7,18 @@ import {
   IonIcon,
 } from '@ionic/react';
 import React from 'react';
+import { ScanSlip } from '../../hooks/OCR';
 
 /**
- * allows user to click on the fab and take a photo
- * @returns take photo component
+ * 
+ * @returns Take picture button and Tesseract produced text
  */
-
 const TakePictureButton: React.FC = () => {
   const { photo, takePhoto } = usePhotoGallery();
+  const Element = ScanSlip(photo);
   return (
     <IonContent>
+      {Element}
       <IonFab vertical="bottom" horizontal="center" slot="fixed">
         <IonFabButton onClick={() => takePhoto()}>
           <IonIcon icon={receipt}></IonIcon>
