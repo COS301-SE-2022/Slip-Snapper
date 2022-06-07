5ad8a6cbc9e90a469fcd731472328c14e0107f34
SeePeeYou <u17110310@tuks.co.za>
2022-06-07 16:07:25 +0200

    :construction: (pwa) service worker fetch event

--- frontend/apps/slip-snapper/src/service-worker.js
+++ frontend/apps/slip-snapper/src/service-worker.js
@@ -11,3 +11,13 @@ event.waitUntil(
 )
 });
 
+self.addEventListener('fetch',(event)=>{
+    event.respondWith(
+        caches.match(event.request)
+        .then(()=>{
+            return fetch(event.request)
+            .catch(()=> caches.match('offline.html'))
+        })
+    )
+});
+
