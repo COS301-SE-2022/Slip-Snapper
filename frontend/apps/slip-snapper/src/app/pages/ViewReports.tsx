80d2864bfb751225afb8c0a2a44a441dee7a2e5a
Regan <u20556455@tuks.co.za>
2022-07-19 14:52:31 +0200

    :sparkles: (S3): Delete files from S3

--- frontend/apps/slip-snapper/src/app/pages/ViewReports.tsx
+++ frontend/apps/slip-snapper/src/app/pages/ViewReports.tsx
@@ -16,15 +16,21 @@ import React from 'react';
 import { NavButtons } from '../components/NavButtons';
 import ReportTotal from '../components/ReportTotal';
 import '../theme/viewReports.css';
+import AWS from 'aws-sdk';
 
-export const mockTotals = [{ timePeriod: "Daily", total: "R200.02", title: "generateDR" }, { timePeriod: "Weekly", total: "R800.02", title: "generateWR" },
-{ timePeriod: "Monthly", total: "R1000.50", title: "generateMR" }]
+export const mockTotals = [
+  { timePeriod: 'Daily', total: 'R200.02', title: 'generateDR' },
+  { timePeriod: 'Weekly', total: 'R800.02', title: 'generateWR' },
+  { timePeriod: 'Monthly', total: 'R1000.50', title: 'generateMR' },
+];
 
-export const mockReports = [{ reportName: "Report #1", reportData: "" }, { reportName: "Report #2", reportData: "" },
-{ reportName: "Report #3", reportData: "" }]
+export const mockReports = [
+  { reportName: 'Report #1', reportData: '' },
+  { reportName: 'Report #2', reportData: '' },
+  { reportName: 'Report #3', reportData: '' },
+];
 
 const ViewReports: React.FC = () => {
-
   return (
     <IonPage>
       <IonHeader>
@@ -36,15 +42,16 @@ const ViewReports: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-
         <IonTitle>Expenditure Totals</IonTitle>
         <IonRow>
           {mockTotals.map((totals, index) => {
             return (
-              <ReportTotal key={index} reportData={[totals.timePeriod, totals.total, totals.title]} />
-            )
-          })
-          }
+              <ReportTotal
+                key={index}
+                reportData={[totals.timePeriod, totals.total, totals.title]}
+              />
+            );
+          })}
         </IonRow>
 
         <IonTitle>All Reports</IonTitle>
@@ -57,29 +64,39 @@ const ViewReports: React.FC = () => {
             return (
               <IonItem color="tertiary">
                 {report.reportName}
-                <IonButton onClick={() => view(report.reportData)} color="success" slot="end" class="viewButton" >
+                <IonButton
+                  onClick={() => view(report.reportData)}
+                  color="success"
+                  slot="end"
+                  class="viewButton"
+                >
                   View
                 </IonButton>
-                <IonButton onClick={() => deleteReport()} fill="solid" slot="end" color="secondary">
+                <IonButton
+                  onClick={() => deleteReport('ChrisDev/Bug_Busters_SRS.pdf')}
+                  fill="solid"
+                  slot="end"
+                  color="secondary"
+                >
                   Delete
                 </IonButton>
               </IonItem>
-            )
-          })
-          }
+            );
+          })}
         </IonCard>
       </IonContent>
     </IonPage>
   );
-  function view(data:any) {
+
+  function view(data: any) {
     if (data.Body !== undefined) {
-      const blob = new Blob([data.Body], { type: "application/pdf" });
+      const blob = new Blob([data.Body], { type: 'application/pdf' });
       const docUrl = URL.createObjectURL(blob);
-      window.open(docUrl)
+      window.open(docUrl);
     }
   }
-  function deleteReport(){
-    console.log(123)
+  function deleteReport(path: string) {
+    return;
   }
 };
 export default ViewReports;
