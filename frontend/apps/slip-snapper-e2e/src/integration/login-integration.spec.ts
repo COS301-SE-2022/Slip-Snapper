f4e9b09b49ac6a290600e7224558627a1134862c
Regan <u20556455@tuks.co.za>
2022-07-25 19:57:38 +0200

    :white_check_mark: (testing): more integration tests

--- frontend/apps/slip-snapper-e2e/src/integration/login-integration.spec.ts
+++ frontend/apps/slip-snapper-e2e/src/integration/login-integration.spec.ts
@@ -1,50 +1,46 @@
-describe('Logging in',()=>{
-  beforeEach(() =>{
-      cy.clearCookies();
-      cy.visit('http://localhost:4200')
-      cy.intercept(
-        {
-          method: 'GET',
-          url: '/api/report/recent*',
-        },
-        { fixture: '../fixtures/RecentReports.json' }
-      )
-      cy.intercept(
-        {
-          method: 'POST',
-          url: '/api/user/login*',
-          times: 1,
-        },
-        { fixture: '../fixtures/Login.json' },
-
-        
-      )
-  })
-
-  it('base url leads to login', ()=>{
-    cy.url().should('eq','http://localhost:4200/login')
-  })
-  it('logs in with the correct email and password', ()=>{
-      cy.get('input[ title="usernameInput"]').type('Regan')
-      cy.get('input[title="passwordInput"]').type('Password123')
-      cy.get('ion-button[type="submit"]').click()
-      cy.url().should('eq','http://localhost:4200/home')
-  })
-
-  it('logs in with the incorrect email and password', ()=>{
+describe('Logging in', () => {
+  beforeEach(() => {
+    cy.clearCookies();
+    cy.visit('http://localhost:4200');
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/api/report/recent*',
+      },
+      { fixture: '../fixtures/RecentReports.json' }
+    );
     cy.intercept(
       {
         method: 'POST',
         url: '/api/user/login*',
         times: 1,
       },
-      { fixture: '../fixtures/InvalidLogin.json' },
-    )
-    cy.get('input[ title="usernameInput"]').type('NotRegan')
-    cy.get('input[title="passwordInput"]').type('Password123')
-    cy.get('ion-button[type="submit"]').click()
-    cy.url().should('eq','http://localhost:4200/login')
-})
+      { fixture: '../fixtures/Login.json' }
+    );
+  });
 
+  it('base url leads to login', () => {
+    cy.url().should('eq', 'http://localhost:4200/login');
+  });
+  it('logs in with the correct email and password', () => {
+    cy.get('input[ title="usernameInput"]').type('Regan');
+    cy.get('input[title="passwordInput"]').type('Password123');
+    cy.get('ion-button[type="submit"]').click();
+    cy.url().should('eq', 'http://localhost:4200/home');
+  });
+
+  it('logs in with the incorrect email and password', () => {
+    cy.intercept(
+      {
+        method: 'POST',
+        url: '/api/user/login*',
+        times: 1,
+      },
+      { fixture: '../fixtures/InvalidLogin.json' }
+    );
+    cy.get('input[ title="usernameInput"]').type('NotRegan');
+    cy.get('input[title="passwordInput"]').type('Password123');
+    cy.get('ion-button[type="submit"]').click();
+    cy.url().should('eq', 'http://localhost:4200/login');
+  });
 });
-
