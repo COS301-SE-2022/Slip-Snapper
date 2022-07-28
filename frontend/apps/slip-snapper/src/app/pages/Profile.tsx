a91606068acaf6efc97fb855494ecfa7c7704aab
Regan <u20556455@tuks.co.za>
2022-07-28 16:17:25 +0200

    :bug: (nav): history usage

--- frontend/apps/slip-snapper/src/app/pages/Profile.tsx
+++ frontend/apps/slip-snapper/src/app/pages/Profile.tsx
@@ -24,12 +24,14 @@ import { setBudgetA, getProfileData } from "../../api/apiCall"
 import Budget from '../components/Budget';
 import { UserStats } from '../components/UserStats';
 import { create } from 'ionicons/icons';
+import { useHistory } from 'react-router-dom';
 
 const Profile: React.FC = () => {
   const [logoutAlert, setLogoutAlert] = useState(false);
   const [weeklyBudgetAlert, setWeeklyBudgetAlert] = useState(false);
   const [monthlyBudgetAlert, setMonthlyBudgetAlert] = useState(false);
   const val = { weekly: 0, monthly: 0 };
+  const history = useHistory();
 
   let totalWeeklySpent = 300;
   let totalMonthlySpent = 500;
@@ -41,13 +43,15 @@ const Profile: React.FC = () => {
     getProfileData(user.id)
       .then(
         apiResponse => {
-          val.weekly = apiResponse.data.weekly;
-          val.monthly = apiResponse.data.monthly;
-          totalWeeklySpent = apiResponse.data.weeklyTotal;
-          totalMonthlySpent = apiResponse.data.monthlyTotal;
-          setWeeklyBudget(val.weekly)
-          setMonthlyBudget(val.monthly)
-          setProfile(apiResponse.data)
+          if(typeof(apiResponse.data) !== "string"){
+            val.weekly = apiResponse.data.weekly;
+            val.monthly = apiResponse.data.monthly;
+            totalWeeklySpent = apiResponse.data.weeklyTotal;
+            totalMonthlySpent = apiResponse.data.monthlyTotal;
+            setWeeklyBudget(val.weekly)
+            setMonthlyBudget(val.monthly)
+            setProfile(apiResponse.data)
+          }
         })
   }, []);
   const [weeklyBudgetValue, setWeeklyBudget] = useState<number>(val.weekly);
@@ -208,7 +212,8 @@ const Profile: React.FC = () => {
   );
 
   function Logout() {
-    throw new Error('Function not implemented.');
+    history.push("/")
+    window.location.reload()
   }
 
   function applyToBudget(newWeeklyBudget: string, newMonthlyBudget: string) {
