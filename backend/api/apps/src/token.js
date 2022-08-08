8d435e4a4a6732242acad6d9a7ff8d70dbb6f76a
Grobbies26 <74908721+Grobbies26@users.noreply.github.com>
2022-08-08 21:49:47 +0200

    📝 (feature/api) Updated functions to have doxygen tags

--- backend/api/apps/src/token.js
+++ backend/api/apps/src/token.js
@@ -1,6 +1,15 @@
 const jwt = require('jsonwebtoken')
 
+/**
+ * Class that handles the JWT tokens
+ */
 class TokenFunctions{
+    //TODO Better handling of errors
+    /**
+     * Function to generate the JWT
+     * @param {*} data The user data to be stored in the token 
+     * @returns The JWT or an error
+     */
     async generateToken(data) {
         try {
             return jwt.sign(
@@ -16,10 +25,17 @@ class TokenFunctions{
         }
     }
 
+    //TODO Better handling of errors
+    /**
+     * Function to verify the JWT
+     * @param {*} token The encoded JWT
+     * @returns The decoded JWT or an error
+     */
     async verifyToken(token) {
         if(!token){
             return new Error("Error! No Token Provided.");
         }
+
         try{
             return jwt.verify(token, process.env.JWT_PRIVATE_KEY);
         }
