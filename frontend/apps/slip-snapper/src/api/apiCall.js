4870104ebdafa0e5dc6a1f0ce4a75883f90eb785
Regan <u20556455@tuks.co.za>
2022-08-21 17:18:31 +0200

    :construction: change to local

--- frontend/apps/slip-snapper/src/api/apiCall.js
+++ frontend/apps/slip-snapper/src/api/apiCall.js
@@ -5,8 +5,8 @@ const headers = {
     'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem('token'))
 }
 
-const baseUrl = 'https://slipsnapper.herokuapp.com/api/'
-//const baseUrl = 'http://localhost:3000/api/'
+//const baseUrl = 'https://slipsnapper.herokuapp.com/api/'
+const baseUrl = 'http://localhost:3000/api/'
 
 /**
  * To process the text extracted by OCR
