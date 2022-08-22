5001cd35da7856843f7b5990bebc28f8600aa0ed
Grobbies26 <74908721+Grobbies26@users.noreply.github.com>
2022-08-22 21:57:17 +0200

    🎨 (feature/categoriser): Generates full dataSet

--- backend/api/apps/itemCategoriser/dataSet_Generation/generator.js
+++ backend/api/apps/itemCategoriser/dataSet_Generation/generator.js
@@ -1,5 +1,7 @@
 const fs = require('fs')
-const csvParser = require('csv-parse')
+const csvParser = require('csv-parse');
+const csvWriter = require('csv-stringify');
+const { resolve } = require('path');
 
 // Original Data Sets
 // https://www.kaggle.com/datasets/lakritidis/product-classification-and-categorization?resource=download
@@ -13,7 +15,7 @@ let allResults = []
 
 async function findUniquesGroceries(){
     let count = { total: 0 };
-
+    
     for( const element of groceriesResults ) {
         
         if( !count[element.itemDescription] ){
@@ -23,7 +25,7 @@ async function findUniquesGroceries(){
             const newElement = {
                 Date: element.Date,
                 Item: element.itemDescription,
-                Location: '',
+                Location: 'unknown',
                 Quantity: 1,
                 Price: 0.0,
                 Category: "Food"
@@ -65,9 +67,9 @@ async function findUniquesPrice(){
             count.total++;
 
             const newElement = {
-                Date: '',
+                Date: 'unknown',
                 Item: element.Cluster,
-                Location: '',
+                Location: 'PriceRunner',
                 Quantity: 1,
                 Price: 0.0,
                 Category: element.Category
@@ -113,9 +115,9 @@ async function findUniquesShop(){
             count.total++;
 
             const newElement = {
-                Date: '',
+                Date: 'unknown',
                 Item: element.ProductTitle,
-                Location: '',
+                Location: 'ShopMania',
                 Quantity: 1,
                 Price: 0.0,
                 Category: element.Category
@@ -161,9 +163,9 @@ async function findUniquesSkrout(){
             count.total++;
 
             const newElement = {
-                Date: '',
+                Date: 'unknown',
                 Item: element.Cluster,
-                Location: '',
+                Location: 'Skroutz',
                 Quantity: 1,
                 Price: 0.0,
                 Category: element.Category
@@ -200,8 +202,135 @@ async function cleanSkroutz(){
     
 }
 
+async function checkCategories(){
+    return new Promise((resolve)=>{
+        let count = { total: 0 };
+        for( const element of allResults ) {
+            if( !count[element.Category] ){
+                count[element.Category] = 1;
+                count.total++;
+                continue;
+            }
+    
+            count[element.Category] = count[element.Category] + 1;
+        }
+        
+        console.table(count)
+        resolve(true)
+    })
+}
+
+async function updateCategories(){
+    return new Promise(async (resolve)=>{
+        let food = [
+            'Food', 'Feeding', 'Alcoholic Drinks', 'Non-alcoholic drinks', 'Sweets & Chocolate'
+        ];
+        let electronis = [ 
+            'Mobile Phones', 'CPUs', 'Digital Cameras', 'Audio / Video Equipment', 'GPS Accessories', 'GPS Receivers', 'Cell Phones Accessories', 'Phones & Faxes',
+            'Smartwatches', 'Cables & Adapters', 'Notebook Accessories', 'PDA Accessories', 'Printer Accessories', 'System Accessories', 'Tablet Accessories',
+            'Card Readers', 'Cases and Power Supplies', 'Cooling Fans', 'Network Adapters', 'UPS', 'Desktops', 'Servers', 'Consumables', 'IT services', 'All-In-Ones',
+            'Gaming Accessories', 'Keyboards & Mice', 'Office software', 'Operating systems', 'Other software', 'Security', 'External Memory', 'SSD', 'Hubs',
+            'Network Accessories', 'Routers', 'Wireless Networking', '3D Multimedia Equipment', 'Audio Systems', 'CD Players', 'DVD & Blu-Ray Players', 'Speakers',
+            'Digital Camera and Camcorder Accessories', 'Audio / DJ Equipment', 'Barcode readers & Accessories', 'Batteries', 'Headphones', 'Microphones', 
+            'Projector Accessories', 'Remote controllers', 'Optical Tools', 'MP3 and MP4 Player Accessories', 'Supports', 'Electrical & Electronic components',
+            'DSLR Cameras', 'Compact Cameras', 'Mirrorless Cameras', 'Analog Cameras'
+        ];
+        let fashion = [ 
+            'Belts', 'Bags & Backpacks', 'Handbags & Wallets', 'Hats and gloves', 'Jewelry', 'Other Accessories', 'Scarves and wraps', 'Sunglasses', 'Watches',
+            'Baby & Children Apparel', 'Men Footwear', 'Men Lingerie', 'Men\'s Clothing', 'Maternity Apparel', 'Women Footwear', 'Women\'s Clothing', 
+            'Women\'s Lingerie', 'Homewear', 'Safety Gear', 'Wedding and Bridal Articles', 'Women\'s Watches', 'Men\'s Watches'
+        ];
+        let household = [ 
+            'TVs', 'Microwaves', 'Dishwashers', 'Washing Machines', 'Freezers', 'Fridge Freezers', 'Fridges', 'Home Decor', 'Toys', 'DVDs', 'Games', 'Music', 
+            'Home Cinema', 'Projectors', 'Home Entertainment', 'Radios', 'Calculators', 'Digital Picture Frames', 'Christmas Items & Decorations', 'Fragrance',
+            'Air Compressors & Accessories', 'Measuring & Layout Tools', 'Plumbing Accessories', 'Plumbing Supplies', 'Power Tools', 'Tools', 'Building supplies',
+            'Furniture', 'Garden & Outdoors', 'Home Improvement', 'Kitchen & Dining', 'Pet Supply', 'Pets', 'Rugs  Tapestry & Linens', 'Solar Panels & Accessories', 
+            'Air Conditioners', 'Air Purifiers', 'Dehumidifiers', 'Fan Heaters', 'Fans', 'Heaters', 'Humidifiers', 'Stoves & Fireplaces', 'Chest Freezers', 
+            'Grills & Ovens', 'Refrigerators', 'Vacuum Cleaners', 'Lighting', 'Bread Makers', 'Coffee Makers & Grinders', 'Food Grinders & Mills', 'Food Steamers',
+            'Ice Cream Makers', 'Juicers', 'Mixers & blenders', 'Other Kitchen Appliances', 'Toasters & Sandwich Makers', 'Irons', 'Other Small Appliances',
+            'Party Supplies', 'Office Equipment', 'Office Supplies', 'Televisions', 'Cookers & Ovens', 'Home Air Conditioners - Inverters'
+        ];
+        let hobby = [ 
+            'Collectibles', 'Crafts', 'Esoteric and Occult Products', 'Books', 'Flowers', 'Body Art', 'Fitness', 'Sewing Machines', 'Board Games', 'Make-up & Costumes',
+            'Adult', 'Gadget', 'Industrial Gear', 'Magazines', 'Musical Instruments', 'Baseball & Softball', 'Basketball', 'Bikes & Accessories', 'Boats & Accessories',
+            'Boxing & Martial Arts', 'Camping', 'Climbing', 'Darts', 'Fishing', 'Football', 'Golf', 'Hockey & Ice Skating', 'Hunting', 'Other Sports', 
+            'Paintball & Airsoft', 'Skateboarding & Skating', 'Snowboarding & Ski', 'Soccer', 'Table tennis', 'Tennis', 'Volleyball', 'Water Sports'
+        ]
+        let vehicle = [
+            'Auto Accessories', 'Car Kits', 'Moto Accessories', 'Motorcycle', 'Wheels', 'Car & Boat Batteries'
+        ]
+        let healthCare = [
+            'Health & Safety', 'Bath & Body', 'Medicine', 'Men Cosmetics', 'Optical Supplies & Eyeglasses', 'Personal Care', 'Various Natural Products',
+            'Vitamins & Supplements', 'Women Cosmetics', 'Cleaning Supplies', 'Hair Care', 'Medical Supplies'
+        ]
+        let other = [
+            'Baby gear', 'Other gifts'
+        ]
+
+        console.log('Updating Categories')
+        for( let i = 0; i < allResults.length; i++ ) {
+            if( food.includes(allResults[i].Category) ){
+                allResults[i].Category = 'Food'
+                continue;
+            }
+
+            if( household.includes(allResults[i].Category) ){
+                allResults[i].Category = 'Household'
+                continue;
+            }
+            
+            if( electronis.includes(allResults[i].Category) ){
+                allResults[i].Category = 'Electronics'
+                continue;
+            }
+
+            if( other.includes(allResults[i].Category) ){
+                allResults[i].Category = 'Other'
+                continue;
+            }
+
+            if( vehicle.includes(allResults[i].Category) ){
+                allResults[i].Category = 'Vehicle'
+                continue;
+            }
+
+            if( healthCare.includes(allResults[i].Category) ){
+                allResults[i].Category = 'HealthCare'
+                continue;
+            }
+
+            if( fashion.includes(allResults[i].Category) ){
+                allResults[i].Category = 'Fashion'
+                continue;
+            }
+
+            if( hobby.includes(allResults[i].Category) ){
+                allResults[i].Category = 'Hobby'
+                continue;
+            }
+        }
+        await checkCategories();
+        resolve(true)
+    })
+}
+
 async function createDataSet(){
-    console.log("finished");
+    return new Promise(async (resolve) => {
+        await updateCategories();
+        
+        const fileName = 'fullDataSet.csv';
+        const file = fs.createWriteStream( fileName );
+        const columns = ['Date', 'Item', 'Location', 'Quantity', 'Price', 'Category'];
+        const stringy = csvWriter.stringify( { header: true, columns: columns } );
+    
+        for(const item of allResults){
+            stringy.write(item)
+        }
+        stringy.pipe(file)
+        console.log("Finished Creating Data Set");
+
+        resolve();
+    })
 }
 
 async function main(){
@@ -209,10 +338,10 @@ async function main(){
     await cleanPrice();
     await cleanShop();
     await cleanSkroutz();
+    console.log("Finished Cleaning All sets. Items:", allResults.length); 
 
-    console.log("Finished Cleaning All sets. Items:", allResults.length)
-
-    
+    await createDataSet();
+    console.log("Dataset has been created and is saved")
 }
 
 main();
