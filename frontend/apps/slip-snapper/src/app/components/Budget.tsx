40e5c76ca604dc889414fe5ecc70dafe693fd20e
KenanDevraj <christiand18200@gmail.com>
2022-08-23 22:21:15 +0200

    🚧 (ui): Render new budgets dynamically

--- frontend/apps/slip-snapper/src/app/components/Budget.tsx
+++ frontend/apps/slip-snapper/src/app/components/Budget.tsx
@@ -12,7 +12,7 @@ import { create } from 'ionicons/icons'
 import { getProfileData, setGeneralBudget } from "../../api/apiCall"
 
 
-let globalCategoryBudgets, globalCategorySpent: any
+let globalCategoryBudgets, globalCategorySpent: any, globalRenderedBudgets: any, globalSetRenderedBudgets: any
 
 function Budget() {
 
@@ -72,8 +72,12 @@ function Budget() {
         },
     });
 
+    const [renderedBudgets, setRenderedBudgets] = useState({ FoodBudget: 0, FashionBudget: 0, ElecBudget: 0, HouseBudget: 0, OtherBudget: 0 })
+
     globalCategoryBudgets = categoryBudgets;
     globalCategorySpent = categorySpent;
+    globalRenderedBudgets = renderedBudgets
+    globalSetRenderedBudgets = setRenderedBudgets
 
     useEffect(() => {
         let user = JSON.parse(localStorage.getItem('user')!)
@@ -84,18 +88,18 @@ function Budget() {
             .then(
                 apiResponse => {
                     if (typeof (apiResponse.data) !== "string") {
+                        console.log(apiResponse.data.otherBudgets.budgets.budgets)
                         setCategoryBudgets(apiResponse.data.otherBudgets.budgets.budgets)
                         setCategorySpent(apiResponse.data.otherBudgets.totals)
                         setProgressBars(apiResponse.data.otherBudgets.budgets.budgets, apiResponse.data.otherBudgets.totals)
                     }
                 })
     }, []);
-
     return (
         <div>
             <IonItem id="foodBudget" className="categoryBudgets" color="tertiary" >
                 <IonIcon className="edit-budget" src={create} onClick={() => setFood(true)} />
-                <IonText>{"Food: R" + categoryBudgets.FoodBudget.weeklyValue}</IonText>
+                <IonText>{"Food: R" + renderedBudgets.FoodBudget}</IonText>
                 <IonProgressBar id='foodBar' class='progressBar' slot="end"></IonProgressBar><br />
             </IonItem>
             <IonAlert
@@ -114,7 +118,7 @@ function Budget() {
 
             <IonItem id="fashionBudget" className="categoryBudgets" color="tertiary">
                 <IonIcon className="edit-budget" src={create} onClick={() => setFashion(true)} />
-                <IonText>{"Fashion: R" + categoryBudgets.FashionBudget.weeklyValue}</IonText>
+                <IonText>{"Fashion: R" + renderedBudgets.FashionBudget}</IonText>
                 <IonProgressBar id='fashionBar' class='progressBar' slot="end"></IonProgressBar><br />
             </IonItem>
             <IonAlert
@@ -133,7 +137,7 @@ function Budget() {
 
             <IonItem id="electronicsBudget" className="categoryBudgets" color="tertiary">
                 <IonIcon className="edit-budget" src={create} onClick={() => setElec(true)} />
-                <IonText>{"Electronics: R" + categoryBudgets.ElectronicsBudget.weeklyValue}</IonText>
+                <IonText>{"Electronics: R" + renderedBudgets.ElecBudget}</IonText>
                 <IonProgressBar id='elecBar' class='progressBar' slot="end"></IonProgressBar><br />
             </IonItem>
             <IonAlert
@@ -152,7 +156,7 @@ function Budget() {
 
             <IonItem id="houseBudget" className="categoryBudgets" color="tertiary">
                 <IonIcon className="edit-budget" src={create} onClick={() => setHouseHold(true)} />
-                <IonText>{"Household: R" + categoryBudgets.HouseholdBudget.weeklyValue}</IonText>
+                <IonText>{"Household: R" + renderedBudgets.HouseBudget}</IonText>
                 <IonProgressBar id='houseBar' class='progressBar' slot="end"></IonProgressBar><br />
             </IonItem>
             <IonAlert
@@ -160,7 +164,7 @@ function Budget() {
                 onDidDismiss={() => setHouseHold(false)}
                 header={'Change Budget'}
                 inputs={[
-                    { name: 'house', placeholder: 'Insert Fashion Budget' },]}
+                    { name: 'house', placeholder: 'Insert Household Budget' },]}
                 buttons={[
                     {
                         text: 'Apply',
@@ -171,7 +175,7 @@ function Budget() {
 
             <IonItem id="otherBudget" className="categoryBudgets" color="tertiary">
                 <IonIcon className="edit-budget" src={create} onClick={() => setOther(true)} />
-                <IonText>{"Other: R" + categoryBudgets.OtherBudget.weeklyValue}</IonText>
+                <IonText>{"Other: R" + renderedBudgets.OtherBudget}</IonText>
                 <IonProgressBar id='otherBar' class='progressBar' slot="end"></IonProgressBar><br />
             </IonItem>
             <IonAlert
@@ -203,7 +207,11 @@ function Budget() {
             }
 
             else {
-                categoryBudgets.FoodBudget.weeklyValue = newBudget
+                if (categoryBudgets.FoodBudget.timeFrame === true) {
+                    categoryBudgets.FoodBudget.weeklyValue = newBudget
+                }
+                else { categoryBudgets.FoodBudget.monthlyValue = newBudget }
+                console.log(categoryBudgets)
                 setCategoryBudgets(categoryBudgets)
                 isExceeded(newBudget, "foodBar", categorySpent.Food)
                 setGeneralBudget(user.id, categoryBudgets)
@@ -218,11 +226,15 @@ function Budget() {
             }
 
             else {
-                categoryBudgets.FashionBudget.weeklyValue = newBudget
+                if (categoryBudgets.FashionBudget.timeFrame === true) {
+                    categoryBudgets.FashionBudget.weeklyValue = newBudget
+                }
+                else { categoryBudgets.FashionBudget.monthlyValue = newBudget }
                 setCategoryBudgets(categoryBudgets)
                 isExceeded(newBudget, "fashionBar", categorySpent.Fashion)
                 setGeneralBudget(user.id, categoryBudgets)
                 present("New Fashion budget set", 1200)
+
             }
         }
 
@@ -233,7 +245,11 @@ function Budget() {
             }
 
             else {
-                categoryBudgets.ElectronicsBudget.weeklyValue = newBudget
+                if (categoryBudgets.ElectronicsBudget.timeFrame === true) {
+                    categoryBudgets.ElectronicsBudget.weeklyValue = newBudget
+
+                }
+                else { categoryBudgets.ElectronicsBudget.monthlyValue = newBudget }
                 setCategoryBudgets(categoryBudgets)
                 isExceeded(newBudget, "elecBar", categorySpent.Electronics)
                 setGeneralBudget(user.id, categoryBudgets)
@@ -248,7 +264,12 @@ function Budget() {
             }
 
             else {
-                categoryBudgets.HouseholdBudget.weeklyValue = newBudget
+                if (categoryBudgets.HouseholdBudget.timeFrame === true) {
+                    categoryBudgets.HouseholdBudget.weeklyValue = newBudget
+                }
+                else {
+                    categoryBudgets.HouseholdBudget.monthlyValue = newBudget
+                }
                 setCategoryBudgets(categoryBudgets)
                 isExceeded(newBudget, "houseBar", categorySpent.household)
                 setGeneralBudget(user.id, categoryBudgets)
@@ -262,17 +283,18 @@ function Budget() {
             }
 
             else {
-                categoryBudgets.OtherBudget.weeklyValue = newBudget
+                if (categoryBudgets.OtherBudget.timeFrame === true) {
+                    categoryBudgets.OtherBudget.weeklyValue = newBudget
+                }
+                else { categoryBudgets.OtherBudget.monthlyValue = newBudget }
                 setCategoryBudgets(categoryBudgets)
                 isExceeded(newBudget, "otherBar", categorySpent.Other)
                 setGeneralBudget(user.id, categoryBudgets)
                 present("New Other budget set", 1200)
             }
         }
+        setProgressBars(categoryBudgets, categorySpent)
     }
-
-
-
 
 }
 
@@ -296,45 +318,65 @@ function isExceeded(budget: number, barID: string, total: number) {
         document.getElementById(barID)?.setAttribute("color", "success")
     }
 }
-
 function setProgressBars(budgets: any, totals: any) {
 
-    console.log(budgets)
+    const newBudgets = { FoodBudget: 0, FashionBudget: 0, ElecBudget: 0, HouseBudget: 0, OtherBudget: 0 }
+
     if (budgets.FoodBudget.timeFrame === true) {
-        isExceeded(budgets.FoodBudget.weeklyValue, "foodBar", totals.Food);       
+        isExceeded(budgets.FoodBudget.weeklyValue, "foodBar", totals.Food);
+        newBudgets.FoodBudget = budgets.FoodBudget.weeklyValue
+
     }
     else {
         isExceeded(budgets.FoodBudget.monthlyValue, "foodBar", totals.Food);
+        newBudgets.FoodBudget = budgets.FoodBudget.monthlyValue
     }
 
     if (budgets.ElectronicsBudget.timeFrame === true) {
         isExceeded(budgets.ElectronicsBudget.weeklyValue, "elecBar", totals.Electronics);
+        newBudgets.ElecBudget = budgets.ElectronicsBudget.weeklyValue
     }
     else {
         isExceeded(budgets.ElectronicsBudget.monthlyValue, "elecBar", totals.Electronics);
+        newBudgets.ElecBudget = budgets.ElectronicsBudget.monthlyValue
+
     }
 
     if (budgets.FashionBudget.timeFrame === true) {
         isExceeded(budgets.FashionBudget.weeklyValue, "fashionBar", totals.Fashion);
+        newBudgets.FashionBudget = budgets.FashionBudget.weeklyValue
+
     }
     else {
         isExceeded(budgets.FashionBudget.monthlyValue, "fashionBar", totals.Fashion);
+        newBudgets.FashionBudget = budgets.FashionBudget.monthlyValue
+
     }
 
     if (budgets.HouseholdBudget.timeFrame === true) {
         isExceeded(budgets.HouseholdBudget.weeklyValue, "houseBar", totals.household);
+        newBudgets.HouseBudget = budgets.HouseholdBudget.weeklyValue
+
     }
     else {
         isExceeded(budgets.HouseholdBudget.monthlyValue, "houseBar", totals.household);
+        newBudgets.HouseBudget = budgets.HouseholdBudget.monthlyValue
+
     }
 
     if (budgets.OtherBudget.timeFrame === true) {
         isExceeded(budgets.OtherBudget.weeklyValue, "otherBar", totals.Other);
+        newBudgets.OtherBudget = budgets.OtherBudget.weeklyValue
+
     }
     else {
-        console.log(budgets.OtherBudget.monthlyValue)
         isExceeded(budgets.OtherBudget.monthlyValue, "otherBar", totals.Other);
+        newBudgets.OtherBudget = budgets.OtherBudget.monthlyValue
+
     }
+
+    globalSetRenderedBudgets(newBudgets)
+
 }
 
 export function updateBudgets() {
