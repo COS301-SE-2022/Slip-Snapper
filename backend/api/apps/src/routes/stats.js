ce8bc134133d3ee56c78c76fd92d6dd181809894
Grobbies26 <74908721+Grobbies26@users.noreply.github.com>
2022-09-21 11:12:16 +0200

    🎨 (feature/validation): validate category budgets

--- backend/api/apps/src/routes/stats.js
+++ backend/api/apps/src/routes/stats.js
@@ -3,6 +3,22 @@ const { body, validationResult, oneOf } = require('express-validator');
 const router = require("express").Router();
 
 /**
+ * Validate that a category budget object is valid
+ * @param {*} category object to be checked
+ * @returns boolean (true if invalid)
+ */
+function validateCategoryBudget(category){
+    return ( 
+        (Object.keys(category).join() != ([ 'active', 'timeFrame', 'weeklyValue', 'monthlyValue' ]).join()) ||
+        (category.weeklyValue == null || category.monthlyValue == null || category.active == null || category.timeFrame == null) || 
+        (category.active != null && typeof(category.active) != 'boolean') ||
+        (category.timeFrame != null && typeof(category.timeFrame) != 'boolean') ||
+        (category.weeklyValue != null && (typeof(category.weeklyValue) != 'number' || category.weeklyValue < 0)) ||
+        (category.monthlyValue != null && (typeof(category.monthlyValue) != 'number' || category.monthlyValue < 0))
+    )
+}
+
+/**
  * Get the users budget
  * Uses the user id to get the items
  */
@@ -11,18 +27,9 @@ router.get('/profile', async (req,res)=>{
     const tokenVerified = await req.app.get('token').verifyToken(token);
 
     if(tokenVerified === "Error"){
-        return res.status(200)
+        return res.status(403)
             .send({
                 message: "Token has expired Login again to continue using the application",
-                weeklyTotal: 0,
-                weekly: 0,
-                monthlyTotal: 0,
-                monthly: 0,
-                favouriteStore: {
-                    name: "",
-                    receipts: [],
-                },
-                otherBudgets: {},
             });
     }
     
@@ -59,15 +66,13 @@ router.post('/budget', async (req,res)=>{
             return res.status(403)
                 .send({
                     message: "Token has expired Login again to continue using the application",
-                    weekly: 0,
-                    monthly: 0,
                 });
         }
 
         let { weekly, monthly } = req.body;
         if( (weekly == null && monthly == null) || 
-            (weekly != null && typeof(weekly) != 'number') ||
-            (monthly != null && typeof(monthly) != 'number') 
+            (weekly != null && (typeof(weekly) != 'number' || weekly < 0)) ||
+            (monthly != null && (typeof(monthly) != 'number' || monthly < 0)) 
         ){
             return res.status(400)
                 .send({
@@ -102,38 +107,37 @@ router.post('/categoryBudgets', async (req, res) => {
     const tokenVerified = await req.app.get('token').verifyToken(token);
 
     if (tokenVerified === "Error") {
-        return res.status(200)
+        return res.status(403)
             .send({
                 message: "Token has expired Login again to continue using the application",
-                budgets: {},
             });
     }
     
     let { budgets } = req.body;
-    // if( (weekly == null && monthly == null) || 
-    //         (weekly != null && typeof(weekly) != 'number') ||
-    //         (monthly != null && typeof(monthly) != 'number') 
-    //     ){
-    //         return res.status(400)
-    //             .send({
-    //                 message: "Missing or Invalid input data",
-    //             });
-    //     }
-
     for (const key in budgets) {
         if (budgets.hasOwnProperty(key)) {
+            console.log(validateCategoryBudget(budgets[key]))
+            if( (!(['FoodBudget', 'FashionBudget', 'ElectronicsBudget', 'HouseholdBudget', 'OtherBudget', 'HobbyBudget', 'HealthcareBudget', 'VehicleBudget'].includes(key))) ||
+                validateCategoryBudget(budgets[key])
+            ){  
+                return res.status(400)
+                    .send({
+                        message: "Missing or Invalid input data",
+                    });
+            }
+
             budgets[key].weeklyValue = parseFloat(budgets[key].weeklyValue)
             budgets[key].monthlyValue = parseFloat(budgets[key].monthlyValue)
         }
     }
 
     const result = await req.app.get('db').updateWeeklyMonthlyCategoryBudgets(Number(tokenVerified.user.id), budgets);
-    let status = 200;
 
-    return res.status(status)
+    return res.status(200)
         .send({
-            message: result.message,
-            budgets: result.budgets,
+            // message: result.message,
+            // budgets: result.budgets,
+            message: "hello"
         });
 });
 
@@ -146,33 +150,15 @@ router.get('', async (req,res)=>{
     const tokenVerified = await req.app.get('token').verifyToken(token);
 
     if(tokenVerified === "Error"){
-        return res.status(200)
+        return res.status(403)
             .send({
                 message: "Token has expired Login again to continue using the application",
-
-                category: {
-                    amount: 0,
-                    name: ""
-                },
-                mostExpensive: {
-                    amount: 0,
-                    name: ""
-                },
-                lastWeek:{
-                    current: 0,
-                    previous: 0
-                },
-                lastMonth:{
-                    current: 0,
-                    previous: 0
-                }
             });
     }
 
     const result = await req.app.get('db').getUserStats( Number(tokenVerified.user.id) );
-    let status = 200;
 
-    return res.status(status)
+    return res.status(200)
         .send({
             message : result.message,
 
@@ -203,21 +189,17 @@ router.get('/today', async (req, res) => {
     const tokenVerified = await req.app.get('token').verifyToken(token);
 
     if(tokenVerified === "Error"){
-        return res.status(200)
+        return res.status(403)
             .send({
                 message: "Token has expired Login again to continue using the application",
-                totalItems: 0,
-                totalSpent: 0
             });
     }
 
     const result = await req.app.get("db").todaysReports(Number(tokenVerified.user.id));
 
-    let status = 200;
-
     //TODO error checking
 
-    return res.status(status)
+    return res.status(200)
         .send({
             message: result.message,
             totalItems: result.sum,
